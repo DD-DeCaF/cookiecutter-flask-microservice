11233958bc8f6154665f2c4033e33bf379c3c7e7
Ali Kaafarani <ali@kvikshaug.no>
2018-06-06 16:34:19 +0200

    docs: add docstring to __init__

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
@@ -87,6 +87,7 @@ class Testing(Default):
     """Testing environment configuration."""
 
     def __init__(self, **kwargs):
+        """Initialize the testing environment configuration."""
         super().__init__(**kwargs)
         self.TESTING = True
 

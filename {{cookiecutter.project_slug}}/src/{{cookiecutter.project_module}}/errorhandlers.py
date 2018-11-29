36dd6004383feb6024af7be58ea8bea8e1bafb90
Ali Kaafarani <ali@kvikshaug.no>
2018-11-29 11:00:44 +0100

    refactor: define handler functions top-level

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/errorhandlers.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/errorhandlers.py
@@ -33,51 +33,55 @@ logger = logging.getLogger(__name__)
 
 
 def init_app(app):
-    @app.errorhandler(422)
-    def handle_webargs_error(error):
-        """
-        Handle webargs parser errors.
+    app.register_error_handler(422, handle_webargs_error)
+    app.register_error_handler(HTTPException, handle_http_error)
+    app.register_error_handler(Exception, handle_uncaught_error)
 
-        Ensures a JSON response including all error messages produced from the
-        parser, that we know to be available on the error object.
 
-        Note that this sets some constrains for manually throwing 422 exceptions
-        as error messages must be made available in the same way.
-        """
-        response = jsonify(error.data['messages'])
+def handle_webargs_error(error):
+    """
+    Handle webargs parser errors.
+
+    Ensures a JSON response including all error messages produced from the
+    parser, that we know to be available on the error object.
+
+    Note that this sets some constrains for manually throwing 422 exceptions
+    as error messages must be made available in the same way.
+    """
+    response = jsonify(error.data['messages'])
+    response.status_code = error.code
+    return response
+
+
+def handle_http_error(error):
+    """
+    Handle HTTPExceptions.
+
+    Include the error description and corresponding status code, known to be
+    available on the werkzeug HTTPExceptions.
+    """
+    # As werkzeug's routing exceptions also inherit from HTTPException,
+    # check for those and allow them to return with redirect responses.
+    if isinstance(error, RoutingException):
+        return error
+    else:
+        response = jsonify({'message': error.description})
         response.status_code = error.code
         return response
 
-    @app.errorhandler(HTTPException)
-    def handle_http_error(error):
-        """
-        Handle HTTPExceptions.
 
-        Include the error description and corresponding status code, known to be
-        available on the werkzeug HTTPExceptions.
-        """
-        # As werkzeug's routing exceptions also inherit from HTTPException,
-        # check for those and allow them to return with redirect responses.
-        if isinstance(error, RoutingException):
-            return error
-        else:
-            response = jsonify({'message': error.description})
-            response.status_code = error.code
-            return response
+def handle_uncaught_error(error):
+    """
+    Handle any uncaught exceptions.
 
-    @app.errorhandler(Exception)
-    def handle_uncaught_error(error):
-        """
-        Handle any uncaught exceptions.
+    Since the handler suppresses the actual exception, log it explicitly to
+    ensure it's logged and recorded in Sentry.
 
-        Since the handler suppresses the actual exception, log it explicitly to
-        ensure it's logged and recorded in Sentry.
-
-        Including the exception message would be helpful, but we would risk
-        leaking sensitive information, so return a generic error message to the
-        client.
-        """
-        logger.error("Uncaught exception", exc_info=sys.exc_info())
-        response = jsonify({'message': "Internal server error"})
-        response.status_code = 500
-        return response
+    Including the exception message would be helpful, but we would risk
+    leaking sensitive information, so return a generic error message to the
+    client.
+    """
+    logger.error("Uncaught exception", exc_info=sys.exc_info())
+    response = jsonify({'message': "Internal server error"})
+    response.status_code = 500
+    return response

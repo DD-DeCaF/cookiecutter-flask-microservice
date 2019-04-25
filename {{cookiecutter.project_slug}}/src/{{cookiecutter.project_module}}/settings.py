2fa57eb3e162080b0538cb69f0ba8d64087e9d2f
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-04-25 10:51:53 +0200

    style: apply black

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
@@ -67,8 +67,8 @@ class Default:
             "formatters": {
                 "simple": {
                     "format": "%(asctime)s [%(levelname)s] [%(name)s] "
-                              "%(filename)s:%(funcName)s:%(lineno)d | "
-                              "%(message)s"
+                    "%(filename)s:%(funcName)s:%(lineno)d | "
+                    "%(message)s"
                 }
             },
             "handlers": {

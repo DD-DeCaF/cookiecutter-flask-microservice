7c6e76ce7d5d5c23bf9917a255672283e20b56b1
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-04-25 17:01:28 +0200

    style: apply QA tools

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_module}}/settings.py
@@ -13,7 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 """Provide settings for different deployment scenarios."""
+
 
 import os
 

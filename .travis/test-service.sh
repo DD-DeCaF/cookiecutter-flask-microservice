43832be6a902f005ef0d34c2ac203e5ca46519bf
Ali Kaafarani <ali@kvikshaug.no>
2019-11-12 12:26:14 +0100

    docs: describe cookiecutter vs wsgi-base test intentions

--- .travis/test-service.sh
+++ .travis/test-service.sh
@@ -1,4 +1,11 @@
 set -eux
+
+# Test the current state of the cookiecutter app and its configuration by
+# creating a temporary service from the template and running the full QA suite
+# in it. Note that the image builds on a wsgi-base image that has already
+# passed QA, so we're not testing the dependencies here - just the code and
+# configuration in the cookiecutter.
+
 cookiecutter --no-input .
 pushd name-of-the-project
 make pip-compile
